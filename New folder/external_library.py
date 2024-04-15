df66d19e18d4b552f445a81ca68e074aa3d2850a
7to11 <160437739+7to11@users.noreply.github.com>
2024-04-15 13:18:03 -0600

    Update external_library.py

--- New folder/external_library.py
+++ New folder/external_library.py
@@ -1,7 +1,7 @@
-# External library
+#library
 
-external_variable = "This is an external variable"
+external_variable = "This is an outside variable"
 external_list = [4, 5, 6]
 
 def external_function():
-    return "This is a function from an external library"
+    return "This is a function from an outside library"
